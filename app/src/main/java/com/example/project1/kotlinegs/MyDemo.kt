1b7f6854563a2ab652f06ed2cefe0b5c8309cb00
seethamshumadduri <maddurianshu@gmail.com>
2024-06-21 01:41:26 +0530

    hello world

---
+++ app/src/main/java/com/example/project1/kotlinegs/MyDemo.kt
@@ -0,0 +1,7 @@
+package com.example.project1.kotlinegs
+
+fun main() {
+    println("hello world")
+}
+
+
---
+++ app/src/main/java/com/example/project1/kotlinegs/Student.kt
@@ -0,0 +1,13 @@
+package com.example.project1.kotlinegs
+
+class Student {
+    constructor(name: String?, age: Int, address: String?) {
+        this.name = name
+        this.age = age
+        this.address = address
+    }
+
+    var name: String? = null
+    var age: Int = 0
+    var address: String? = null
+}
